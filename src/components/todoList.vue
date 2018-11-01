25ff01557da91b097012b997cda38d7e7df541c9
xueyuwan <1213287572@qq.com>
2018-11-01 18:16:30 +0800

    todolist

---
+++ src/components/todoList.vue
@@ -0,0 +1,13 @@
+<template>
+     <li @click="handleClick">{{content}}</li>
+</template>
+<script>
+    export default {
+        props:["content","index"],
+        methods:{
+            handleClick:function(){
+                this.$emit("delete",this.index)
+            }
+        }
+    }
+</script>
